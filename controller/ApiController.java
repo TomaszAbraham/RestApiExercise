a0137381c30072b9dc56a0da5209739c356a93b0
TomaszAbraham <112939236+TomaszAbraham@users.noreply.github.com>
2022-11-13 22:22:13 +0100

    Add files via upload

--- controller/ApiController.java
+++ controller/ApiController.java
@@ -3,6 +3,7 @@ package pl.training.restapi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.training.restapi.services.Calculator;
@@ -10,14 +11,27 @@ import pl.training.restapi.services.Calculator;
 
 @RestController
 public class ApiController {
-    private Calculator calculator;
     @GetMapping("/test") 
     public String getResult() {
         return "I am alive!";
         //throw new IllegalArgumentException("not implemented");
     }
     @GetMapping("/calculate/{operation}")
-    public Double calculate(@PathVariable String operation, @RequestParam Integer a, @RequestParam Integer b ) {
-        return calculator.calculate(operation, a, b);
+    @ResponseBody
+    public String calculate(@PathVariable String operation, @RequestParam Integer a, @RequestParam Integer b ) {
+        return "Result: " + Calculator.calculate(operation, a, b).toString();
     }
+    /*public String calculate(@PathVariable String operation, @RequestParam Integer a, @RequestParam Integer b) {
+        switch (operation) {
+            case "add": return String.valueOf (a + b);
+            case "substract": return String.valueOf(a - b);
+            case "multiply": return String.valueOf (a * b);
+            case "dividet": {
+                if (b != 0) return String.valueOf (a / b);
+                else throw new IllegalArgumentException("Can not resolve division by zero!");
+            }
+        }
+        return "error";
+    }*/
+     
 }
--- services/Calculator.java
+++ services/Calculator.java
@@ -1,21 +1,21 @@
 package pl.training.restapi.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public final class Calculator {
-    private Double result;
-    public Double getResult() {
-        return result;
-    }
-    public Double calculate(String operation, Integer a, Integer b) {
+
+    public static Double calculate(String operation, Integer a, Integer b) {
         switch (operation) {
-            case "add": this.result =  (double) (a + b);
-            case "substract": this.result = (double) (a - b);
-            case "multiply": this.result = (double) (a * b);
-            case "dividet": {
-                if (b != 0) this.result = (double) (a / b);
+            case "add": return (double) (a + b);
+            case "substract": return (double) (a - b);
+            case "multiply": return (double) (a * b);
+            case "divide": {
+                if (b != 0) return (double) ((double)a / (double)b);
                 else throw new IllegalArgumentException("Can not resolve division by zero!");
             }
         }
-        return this.result;
+        throw new IllegalArgumentException("error");
     }
 
 
